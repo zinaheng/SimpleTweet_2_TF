5a759ad4594d1c4663093a7eb33d4afcd7b290a8
zinaheng <Zinah31it@yahoo.com>
2021-07-23 23:34:39 -0500

    first commit

--- simpleTweet2/app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
+++ simpleTweet2/app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
@@ -1,2 +1,96 @@
-package com.codepath.apps.restclienttemplate;public class TweetsAdapter {
+package com.codepath.apps.restclienttemplate;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.codepath.apps.restclienttemplate.models.Tweet;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.text.BreakIterator;
+import java.util.List;
+
+public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder>{
+
+    Context context;
+    List<Tweet> tweets;
+    TextView tvTimeStamp;
+    //pass in the context and list of tweets
+
+    public TweetsAdapter(Context context, List<Tweet> tweets) {
+        this.context = context;
+        this.tweets = tweets;
+    }
+
+    //for each row,inflate a layout
+    @NonNull
+    @NotNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_tweet, parent, false);
+
+        return new ViewHolder(view);
+    }
+    //bind values based on postions
+    @Override
+    public void onBindViewHolder(@NonNull @NotNull ViewHolder holder, int position) {
+        //get the data passed in
+        Tweet tweet = tweets.get(position);
+
+
+        //tvTimeStamp.setText(tweet.getFormattedTimestamp());
+        //bind the tweet with the VH
+        holder.bind(tweet);
+    }
+
+    @Override
+    public int getItemCount() {
+        return tweets.size();
+    }
+    // Clean all elements of the recycler
+
+    public void clear(){
+        tweets.clear();
+        notifyDataSetChanged();
+    }
+    // Add a list of items -- change to type used
+
+    public void addAll(List<Tweet> tweetsList){
+        tweets.addAll(tweetsList);
+        notifyDataSetChanged();
+    }
+
+    //define viewHolder
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView ivProfileImage;
+        TextView tvBody;
+        TextView tvScreenName;
+        TextView tvTimeStamp;
+
+        public ViewHolder(@NonNull @NotNull View itemView) {
+            super(itemView);
+            ivProfileImage = itemView.findViewById(R.id.ivPorfileImage);
+            tvBody = itemView.findViewById(R.id.tvBody);
+            tvScreenName = itemView.findViewById(R.id.tvScreenName);
+            tvTimeStamp = itemView.findViewById(R.id.tvTimeStamp);
+
+        }
+
+        public void bind(Tweet tweet) {
+            tvBody.setText(tweet.body);
+            tvScreenName.setText(tweet.user.screenName);
+            tvTimeStamp.setText(tweet.getFormattedTimestamp());
+            Glide.with(context).load(tweet.user.profileImageUrl).into(ivProfileImage);
+        }
+    }
 }
--- simpleTweet2/app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
+++ simpleTweet2/app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -1,2 +1,41 @@
-package com.codepath.apps.restclienttemplate.models;public class Tweet {
+package com.codepath.apps.restclienttemplate.models;
+
+import com.codepath.apps.restclienttemplate.TimeFormatter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tweet {
+    private static String createdAt;
+    public String body;
+
+    public User user;
+    public long id;
+
+    public static Tweet fromJson(JSONObject jsonObject) throws JSONException {
+        Tweet tweet = new Tweet();
+        tweet.body = jsonObject.getString("text");
+        tweet.createdAt = jsonObject.getString("created_at");
+        tweet.user = User.fromJson(jsonObject.getJSONObject("user"));
+        tweet.createdAt = jsonObject.getString("created_at");
+        tweet.id = jsonObject.getLong("id");
+
+        return tweet;
+    }
+    public static List<Tweet> fromJsonArray (JSONArray jsonArray) throws JSONException {
+        List<Tweet> tweets = new ArrayList<>();
+        for(int i =0; i<jsonArray.length();i++){
+            tweets.add(fromJson((jsonArray.getJSONObject(i))));
+        }
+        return tweets;
+    }
+
+
+    public String getFormattedTimestamp() {
+        return TimeFormatter.getTimeDifference(createdAt);
+    }
 }
