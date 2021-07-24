ac2b60945786808bd6aea117580a6a20de3738e8
zinaheng <Zinah31it@yahoo.com>
2021-07-24 00:14:43 -0500

    first commit

---
+++ TwitterClone/app/build/generated/source/buildConfig/debug/com/codepath/apps/restclienttemplate/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.codepath.apps.restclienttemplate;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.codepath.apps.restclienttemplate";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from default config.
+  public static final String CONSUMER_KEY = "cc1PxuR4fWN3rFa02NaWDqO20";
+  // Field from default config.
+  public static final String CONSUMER_SECRET = "qLwGYCUa18YqmfwmubneIAkiip4Cbz9z171bXQ2V60vHQMj9ZZ";
+}
